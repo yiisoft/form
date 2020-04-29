7a3397076184248581a1a57c612e5d14ae3fdb06
Alexander Makarov <sam@rmcreative.ru>
2020-04-29 22:03:38 +0300

    Merge FieldOptions into Field not to hide complexity

--- src/Widget/Field.php
+++ src/Widget/Field.php
@@ -7,6 +7,7 @@ namespace Yiisoft\Form\Widget;
 use InvalidArgumentException;
 use Yiisoft\Arrays\ArrayHelper;
 use Yiisoft\Factory\Exceptions\InvalidConfigException;
+use Yiisoft\Form\FormModelInterface;
 use Yiisoft\Form\Helper\HtmlForm;
 use Yiisoft\Html\Html;
 use Yiisoft\Widget\Widget;
@@ -15,7 +16,23 @@ use function array_merge;
 
 class Field extends Widget implements FieldInterface
 {
-    use Collection\FieldsOptions;
+    private ?FormModelInterface $data = null;
+    private string $attribute;
+    private array $options = [];
+    private array $inputOptions = [];
+    private array $labelOptions = [];
+    private bool $ariaAttribute = true;
+    private string $errorCss = 'has-error';
+    private string $errorSummaryCss = 'error-summary';
+    private string $inputCss = 'form-control';
+    private string $requiredCss = 'required';
+    private string $successCss = 'has-success';
+    private string $template = "{label}\n{input}\n{hint}\n{error}";
+    private string $validatingCss = 'validating';
+    private string $validationStateOn = 'input';
+    private ?string $inputId = null;
+    private array $parts = [];
+    private bool $skipForInLabel = false;
 
     public const DIV_CSS = ['class' => 'form-group'];
     public const ERROR_CSS = ['class' => 'help-block'];
@@ -733,5 +750,175 @@ class Field extends Widget implements FieldInterface
     public function addInputId(): string
     {
         return $this->inputId ?: HtmlForm::getInputId($this->data, $this->attribute);
+    }
+
+    public function config(FormModelInterface $data, string $attribute, array $options = []): self
+    {
+        $new = clone $this;
+        $new->data = $data;
+        $new->attribute = $attribute;
+        $new->options = $options;
+        return $new;
+    }
+
+    public function ariaAttribute(bool $value): self
+    {
+        $new = clone $this;
+        $new->ariaAttribute = $value;
+        return $new;
+    }
+
+    public function errorCss(string $value): self
+    {
+        $new = clone $this;
+        $new->errorCss = $value;
+        return $new;
+    }
+
+    public function errorSummaryCss(string $value): self
+    {
+        $new = clone $this;
+        $new->errorSummaryCss = $value;
+        return $new;
+    }
+
+    public function inputCss(string $value): self
+    {
+        $new = clone $this;
+        $new->inputCss = $value;
+        return $new;
+    }
+
+    public function requiredCss(string $value): self
+    {
+        $new = clone $this;
+        $new->requiredCss = $value;
+        return $new;
+    }
+
+    public function successCss(string $value): self
+    {
+        $new = clone $this;
+        $new->successCss = $value;
+        return $new;
+    }
+
+    public function template(string $value): self
+    {
+        $new = clone $this;
+        $new->template = $value;
+        return $new;
+    }
+
+    public function validatingCss(string $value): self
+    {
+        $new = clone $this;
+        $new->validatingCss = $value;
+        return $new;
+    }
+
+    public function validationStateOn(string $value): self
+    {
+        $new = clone $this;
+        $new->validationStateOn = $value;
+        return $new;
+    }
+
+    private function addErrorCssClassToContainer(): void
+    {
+        if ($this->validationStateOn === 'container') {
+            Html::addCssClass($this->options, $this->errorCss);
+        }
+    }
+
+    private function addErrorCssClassToInput(): void
+    {
+        if ($this->validationStateOn === 'input') {
+            $attributeName = Html::getAttributeName($this->attribute);
+
+            if ($this->data->hasErrors($attributeName)) {
+                Html::addCssClass($this->inputOptions, $this->errorCss);
+            }
+        }
+    }
+
+    private function addErrorCssClass(array $options = []): void
+    {
+        $class = $options['class'] ?? self::ERROR_CSS['class'];
+
+        if ($class !== self::ERROR_CSS['class']) {
+            $class = self::ERROR_CSS['class'] . ' ' . $options['class'];
+        }
+
+        Html::addCssClass($this->inputOptions, $class);
+    }
+
+    private function addHintCssClass(array $options = []): void
+    {
+        $class = $options['class'] ?? self::HINT_CSS['class'];
+
+        if ($class !== self::HINT_CSS['class']) {
+            $class = self::HINT_CSS['class'] . ' ' . $options['class'];
+        }
+
+        Html::addCssClass($this->inputOptions, $class);
+    }
+
+    private function addInputCssClass(array $options = []): void
+    {
+        $class = $options['class'] ?? $this->inputCss;
+
+        if ($class !== $this->inputCss) {
+            $class = $this->inputCss . ' ' . $options['class'];
+        }
+
+        Html::addCssClass($this->inputOptions, $class);
+    }
+
+    private function addLabelCssClass(array $options = []): void
+    {
+        $class = $options['class'] ?? self::LABEL_CSS['class'];
+
+        if ($class !== self::LABEL_CSS['class']) {
+            $class = self::LABEL_CSS['class'] . ' ' . $options['class'];
+        }
+
+        Html::addCssClass($this->inputOptions, $class);
+    }
+
+    private function setInputRole(array $options = []): void
+    {
+        $this->inputOptions['role'] = $options['role'] ?? 'radiogroup';
+    }
+
+    private function skipForInLabel(): void
+    {
+        if ($this->skipForInLabel) {
+            $this->inputOptions['for'] = null;
+        }
+    }
+
+    private function setForInLabel(array $options = []): void
+    {
+        if (isset($options['id'])) {
+            $this->inputId = $options['id'];
+
+            if (!isset($new->labelOptions['for'])) {
+                $this->labelOptions['for'] = $options['id'];
+            }
+        }
+    }
+
+    private function setAriaAttributes(array $options = []): void
+    {
+        if ($this->ariaAttribute && ($this->data instanceof FormModelInterface)) {
+            if (!isset($options['aria-required']) && $this->data->isAttributeRequired($this->attribute)) {
+                $this->inputOptions['aria-required'] = 'true';
+            }
+
+            if (!isset($options['aria-invalid']) && $this->data->hasErrors($this->attribute)) {
+                $this->inputOptions['aria-invalid'] = 'true';
+            }
+        }
     }
 }
