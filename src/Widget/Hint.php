443ff91b967fbc285aada892e9ab0ad0bfd0de5c
terabytesoftw <terabytesoftw@gmail.com>
2020-05-02 13:52:35 -0400

    Add methods() specific for Hint::class.

--- src/Widget/Hint.php
+++ src/Widget/Hint.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Widget;
 
 use Yiisoft\Arrays\ArrayHelper;
+use Yiisoft\Form\FormModelInterface;
 use Yiisoft\Html\Html;
 use Yiisoft\Widget\Widget;
 
 final class Hint extends Widget
 {
-    use Options\Common;
+    private FormModelInterface $data;
+    private string $attribute;
+    private array $options = [];
 
     /**
      * Generates a hint tag for the given form attribute.
@@ -28,6 +31,60 @@ final class Hint extends Widget
         }
 
         $tag = ArrayHelper::remove($new->options, 'tag', 'div');
+
         return Html::tag($tag, $hint, $new->options);
+    }
+
+    /**
+     * Configure the FormModel options for the widget.
+     *
+     * @param FormModelInterface $data Represents the {@see FormModel}.
+     * @param string $attribute It is the property defined in the {@see FormModel}.
+     * @param array $options The HTML attributes for the widget container tag. The following special options are
+     * recognized. {@see \Yiisoft\Html\Html::renderTagAttributes()} for details on how attributes are being rendered.
+     *
+     * @return self
+     */
+    public function config(FormModelInterface $data, string $attribute, array $options = []): self
+    {
+        $new = clone $this;
+        $new->data = $data;
+        $new->attribute = $attribute;
+        $new->options = $options;
+        return $new;
+    }
+
+    /**
+     * This specifies the hint to be displayed.
+     *
+     * Note that this will NOT be encoded.
+     * If this is not set, {@see \Yiisoft\Form\FormModel::getAttributeHint()} will be called to get the hint for display
+     * (without encoding).
+     *
+     * @param string $value
+     *
+     * @return self
+     */
+    public function hint(string $value): self
+    {
+        $new = clone $this;
+        $new->options['hint'] = $value;
+        return $new;
+    }
+
+    /**
+     * The tag name of the container element.
+     *
+     * Null to render hint without container {@see Html::tag()}.
+     *
+     * @param string|null $value
+     *
+     * @return self
+     */
+    public function tag(?string $value = null): self
+    {
+        $new = clone $this;
+        $new->options['tag'] = $value;
+        return $new;
     }
 }
--- tests/Widget/HintTest.php
+++ tests/Widget/HintTest.php
@@ -5,20 +5,61 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Tests\Widget;
 
 use Yiisoft\Form\Tests\TestCase;
-use Yiisoft\Form\Tests\Stub\StubForm;
+use Yiisoft\Form\Tests\Stub\PersonalForm;
 use Yiisoft\Form\Widget\Hint;
 
 final class HintTest extends TestCase
 {
     public function testHint(): void
     {
-        $form = new StubForm();
-        $expected = '<div>Enter your name.</div>';
-        $created = Hint::widget()->config($form, 'fieldString')->run();
-        $this->assertEquals($expected, $created);
+        $data = new PersonalForm();
 
-        $expected = '<div class="test">Enter your name.</div>';
-        $created = Hint::widget()->config($form, 'fieldString', ['class' => 'test'])->run();
-        $this->assertEquals($expected, $created);
+        $expected = '<div>Write your first name.</div>';
+        $html = Hint::widget()
+            ->config($data, 'name')
+            ->run();
+        $this->assertEquals($expected, $html);
+    }
+
+    public function testHintOptions(): void
+    {
+        $data = new PersonalForm();
+
+        $expected = '<div class="customClass">Write your first name.</div>';
+        $html = Hint::widget()
+            ->config($data, 'name', ['class' => 'customClass'])
+            ->run();
+        $this->assertEquals($expected, $html);
+    }
+
+    public function testHintCustomHint(): void
+    {
+        $data = new PersonalForm();
+
+        $expected = '<div>Custom hint text.</div>';
+        $html = Hint::widget()
+            ->config($data, 'name')
+            ->hint('Custom hint text.')
+            ->run();
+        $this->assertEquals($expected, $html);
+    }
+
+    public function testHintTag(): void
+    {
+        $data = new PersonalForm();
+
+        $expected = 'Write your first name.';
+        $html = Hint::widget()
+            ->config($data, 'name')
+            ->tag()
+            ->run();
+        $this->assertEquals($expected, $html);
+
+        $expected = '<span>Write your first name.</span>';
+        $html = Hint::widget()
+            ->config($data, 'name')
+            ->tag('span')
+            ->run();
+        $this->assertEquals($expected, $html);
     }
 }
