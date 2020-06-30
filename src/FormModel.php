9edc41b4215002a92852313d68a544c596d72179
Dmitriy Derepko <xepozz@list.ru>
2020-06-30 22:53:23 +0300

    Fix other mutants

--- src/FormModel.php
+++ src/FormModel.php
@@ -84,7 +84,7 @@ abstract class FormModel implements FormModelInterface
     {
         return strpos(static::class, '@anonymous') !== false
             ? ''
-            : mb_substr(mb_strrchr(static::class, "\\"), 1);
+            : substr(strrchr(static::class, "\\"), 1);
     }
 
     public function hasAttribute(string $attribute): bool
@@ -210,6 +210,9 @@ abstract class FormModel implements FormModelInterface
                     case 'int':
                         $this->writeProperty($name, (int) $value);
                         break;
+                    case 'string':
+                        $this->writeProperty($name, (string) $value);
+                        break;
                     default:
                         $this->writeProperty($name, $value);
                         break;
@@ -244,10 +247,13 @@ abstract class FormModel implements FormModelInterface
         $this->attributesErrors[$attribute][] = $error;
     }
 
+    /**
+     * @param string[][] $items
+     */
     private function addErrors(array $items): void
     {
         foreach ($items as $attribute => $errors) {
-            foreach ((array)$errors as $error) {
+            foreach ($errors as $error) {
                 $this->attributesErrors[$attribute][] = $error;
             }
         }
@@ -272,12 +278,13 @@ abstract class FormModel implements FormModelInterface
                 continue;
             }
 
-            $type = (new \ReflectionProperty($property->class, $property->name))->getType();
-
+            $type = $property->getType();
             if ($type === null) {
-                throw new \InvalidArgumentException(
-                    "You must specify the type hint for \"$property->class\" class."
-                );
+                throw new \InvalidArgumentException(sprintf(
+                    'You must specify the type hint for "%s" property in "%s" class.',
+                    $property->getName(),
+                    $property->getDeclaringClass()->getName(),
+                ));
             }
 
             $attributes[$property->getName()] = $type->getName();
