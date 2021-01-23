5f487aa2c3603597e63d893acf7328371fc0cb83
Alexander Makarov <sam@rmcreative.ru>
2021-01-23 22:47:59 +0300

    Add phpdoc for interface

--- src/HtmlOptions/HtmlOptionsProvider.php
+++ src/HtmlOptions/HtmlOptionsProvider.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Form\HtmlOptions;
 
+/**
+ * Provides options for HTML input tags.
+ */
 interface HtmlOptionsProvider
 {
+    /**
+     * @return array Options for HTML input tags in `['optionName' => 'optionValue']` format.
+     */
     public function getHtmlOptions(): array;
 }
