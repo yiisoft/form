2558098f23ac6311fef0d9593222e66e66de61f2
Alexander Makarov <sam@rmcreative.ru>
2021-01-26 19:37:51 +0300

    Update trait to latest validator changes

--- src/HtmlOptions/RuleAwareTrait.php
+++ src/HtmlOptions/RuleAwareTrait.php
@@ -7,13 +7,14 @@ namespace Yiisoft\Form\HtmlOptions;
 use Yiisoft\Validator\DataSetInterface;
 use Yiisoft\Validator\ParametrizedRuleInterface;
 use Yiisoft\Validator\Result;
+use Yiisoft\Validator\ValidationContext;
 
 trait RuleAwareTrait
 {
     protected ParametrizedRuleInterface $rule;
 
-    public function validate($value, DataSetInterface $dataSet = null, bool $previousRulesErrored = false): Result
+    public function validate($value, ValidationContext $context = null): Result
     {
-        return $this->rule->validate($value, $dataSet, $previousRulesErrored);
+        return $this->rule->validate($value, $context);
     }
 }
