d9684a57accefb0036b9bb59a155f8417605e97b
Wilmer <terabytesoftw@gmail.com>
2021-08-04 12:59:14 -0400

    Update Error::class.

--- src/Widget/Error.php
+++ src/Widget/Error.php
@@ -5,65 +5,10 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Widget;
 
 use Yiisoft\Arrays\ArrayHelper;
-use Yiisoft\Form\FormModelInterface;
-use Yiisoft\Form\Helper\HtmlForm;
-use Yiisoft\Html\Html;
-use Yiisoft\Widget\Widget;
+use Yiisoft\Html\Tag\CustomTag;
 
 final class Error extends Widget
 {
-    private FormModelInterface $data;
-    private string $attribute;
-    private array $options = [];
-
-    /**
-     * Generates a tag that contains the first validation error of the specified form attribute.
-     *
-     * @return string the generated label tag
-     */
-    public function run(): string
-    {
-        $new = clone $this;
-
-        $errorSource = ArrayHelper::remove($new->options, 'errorSource');
-
-        if ($errorSource !== null) {
-            $error = $errorSource($new->data, $new->attribute);
-        } else {
-            $error = $new->data->getFirstError(HtmlForm::getAttributeName($new->attribute));
-        }
-
-        $tag = ArrayHelper::remove($new->options, 'tag', 'div');
-        if (empty($tag)) {
-            return $error;
-        }
-
-        $encode = ArrayHelper::remove($new->options, 'encode', true);
-
-        return Html::tag($tag, $error, $new->options)
-            ->encode($encode)
-            ->render();
-    }
-
-    /**
-     * Set form model, name and options for the widget.
-     *
-     * @param FormModelInterface $data Form model.
-     * @param string $attribute Form model property this widget is rendered for.
-     * @param array $options The HTML attributes for the widget container tag.
-     * See {@see \Yiisoft\Html\Html::renderTagAttributes()} for details on how attributes are being rendered.
-     *
-     * @return self
-     */
-    public function config(FormModelInterface $data, string $attribute, array $options = []): self
-    {
-        $new = clone $this;
-        $new->data = $data;
-        $new->attribute = $attribute;
-        $new->options = $options;
-        return $new;
-    }
-
     /**
      * Callback that will be called to obtain an error message.
      *
@@ -80,23 +25,7 @@ final class Error extends Widget
     public function errorSource(array $value = []): self
     {
         $new = clone $this;
-        $new->options['errorSource'] = $value;
-        return $new;
-    }
-
-    /**
-     * Whether to HTML-encode the error messages.
-     *
-     * Defaults to true. This option is ignored if item option is set.
-     *
-     * @param bool $value
-     *
-     * @return self
-     */
-    public function noEncode(bool $value = false): self
-    {
-        $new = clone $this;
-        $new->options['encode'] = $value;
+        $new->attributes['errorSource'] = $value;
         return $new;
     }
 
@@ -112,7 +41,35 @@ final class Error extends Widget
     public function tag(?string $value = null): self
     {
         $new = clone $this;
-        $new->options['tag'] = $value;
+        $new->attributes['tag'] = $value;
         return $new;
+    }
+
+    /**
+     * Generates a tag that contains the first validation error of the specified form attribute.
+     *
+     * @return string the generated label tag
+     */
+    protected function run(): string
+    {
+        $new = clone $this;
+
+        $errorSource = ArrayHelper::remove($new->attributes, 'errorSource');
+
+        if ($errorSource !== null) {
+            $error = $errorSource($new->getFormModel(), $new->getAttribute());
+        } else {
+            $error = $new->getFirstError();
+        }
+
+        $tag = ArrayHelper::remove($new->attributes, 'tag', 'div');
+
+        if (empty($tag)) {
+            return $error;
+        }
+
+        $encode = ArrayHelper::remove($new->attributes, 'encode', true);
+
+        return CustomTag::name($tag)->attributes($new->attributes)->content($error)->encode($encode)->render();
     }
 }
--- tests/Widget/ErrorTest.php
+++ tests/Widget/ErrorTest.php
@@ -4,109 +4,74 @@ declare(strict_types=1);
 
 namespace Yiisoft\Form\Tests\Widget;
 
+use PHPUnit\Framework\TestCase;
 use Yiisoft\Form\Tests\Stub\PersonalForm;
 use Yiisoft\Form\Tests\Stub\ValidatorMock;
-use Yiisoft\Form\Tests\TestCase;
 use Yiisoft\Form\Widget\Error;
+use Yiisoft\Test\Support\Container\SimpleContainer;
 use Yiisoft\Validator\ValidatorInterface;
+use Yiisoft\Widget\WidgetFactory;
 
 final class ErrorTest extends TestCase
 {
     private PersonalForm $data;
     private array $record = [];
 
+    public function testErrorSource(): void
+    {
+        $validator = $this->createValidatorMock();
+        $this->data->load($this->record);
+        $validator->validate($this->data);
+        $this->assertSame(
+            '<div>This is custom error message.</div>',
+            Error::widget()->config($this->data, 'name')->errorSource([$this->data, 'customError'])->render(),
+        );
+    }
+
+    public function testNoEncode(): void
+    {
+        $validator = $this->createValidatorMock();
+        $this->data->load($this->record);
+        $validator->validate($this->data);
+        $this->assertSame(
+            '<div>(&#10006;) This is custom error message.</div>',
+            Error::widget()->config($this->data, 'name', ['encode' => false])->errorSource([$this->data, 'customErrorWithIcon'])->render(),
+        );
+    }
+
+    public function testRender(): void
+    {
+        $validator = $this->createValidatorMock();
+        $this->data->load($this->record);
+        $validator->validate($this->data);
+        $this->assertSame('<div>Value cannot be blank.</div>', Error::widget()->config($this->data, 'name')->render());
+    }
+
+    public function testTag(): void
+    {
+        $validator = $this->createValidatorMock();
+        $this->data->load($this->record);
+        $validator->validate($this->data);
+        $this->assertSame('Value cannot be blank.', Error::widget()->config($this->data, 'name')->tag()->render());
+        $this->assertSame(
+            '<span>Value cannot be blank.</span>',
+            Error::widget()->config($this->data, 'name')->tag('span')->render(),
+        );
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
+        WidgetFactory::initialize(new SimpleContainer(), []);
 
         $this->data = new PersonalForm();
+
         $this->record = [
             'PersonalForm' =>
             [
                 'name' => null,
             ],
         ];
-    }
-
-    public function testError(): void
-    {
-        $validator = $this->createValidatorMock();
-        $this->data->load($this->record);
-
-        $validator->validate($this->data);
-
-        $expected = '<div>Value cannot be blank.</div>';
-        $html = Error::widget()
-            ->config($this->data, 'name')
-            ->run();
-        $this->assertEquals($expected, $html);
-    }
-
-    public function testErrorOptions(): void
-    {
-        $validator = $this->createValidatorMock();
-        $this->data->load($this->record);
-
-        $validator->validate($this->data);
-
-        $expected = '<div class="customClass">Value cannot be blank.</div>';
-        $html = Error::widget()
-            ->config($this->data, 'name', ['class' => 'customClass'])
-            ->run();
-        $this->assertEquals($expected, $html);
-    }
-
-    public function testErrorErrorSource(): void
-    {
-        $validator = $this->createValidatorMock();
-        $this->data->load($this->record);
-
-        $validator->validate($this->data);
-
-        $expected = '<div>This is custom error message.</div>';
-        $html = Error::widget()
-            ->config($this->data, 'name')
-            ->errorSource([$this->data, 'customError'])
-            ->run();
-        $this->assertEquals($expected, $html);
-    }
-
-    public function testErrorNoEncode(): void
-    {
-        $validator = $this->createValidatorMock();
-        $this->data->load($this->record);
-
-        $validator->validate($this->data);
-
-        $expected = '<div>(&#10006;) This is custom error message.</div>';
-        $html = Error::widget()
-            ->config($this->data, 'name')
-            ->errorSource([$this->data, 'customErrorWithIcon'])
-            ->noEncode()
-            ->run();
-        $this->assertEquals($expected, $html);
-    }
-
-    public function testErrorTag(): void
-    {
-        $validator = $this->createValidatorMock();
-        $this->data->load($this->record);
-
-        $validator->validate($this->data);
-
-        $expected = 'Value cannot be blank.';
-        $html = Error::widget()
-            ->config($this->data, 'name')
-            ->tag()
-            ->run();
-        $this->assertEquals($expected, $html);
-
-        $expected = '<span>Value cannot be blank.</span>';
-        $html = Error::widget()
-            ->config($this->data, 'name')
-            ->tag('span')
-            ->run();
-        $this->assertEquals($expected, $html);
     }
 
     private function createValidatorMock(): ValidatorInterface
