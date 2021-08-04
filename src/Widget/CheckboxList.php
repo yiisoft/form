0f014b2cc1d0d9cee3215cc0c8060f9e55c03c49
Wilmer <terabytesoftw@gmail.com>
2021-08-04 11:26:14 -0400

    Update CheckboxList::class.

---
+++ src/Widget/CheckboxList.php
@@ -0,0 +1,223 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Form\Widget;
+
+use Closure;
+use Stringable;
+use Yiisoft\Html\Widget\CheckboxList\CheckboxItem;
+use Yiisoft\Html\Widget\CheckboxList\CheckboxList as ChecboxListWidget;
+
+/*
+ * Generates a list of checkboxes.
+ *
+ * A checkbox list allows multiple selection, like {@see ListBox}.
+ */
+final class CheckboxList extends Widget
+{
+    private array $containerAttributes = [];
+    private ?string $containerTag = 'div';
+    /** @psalm-var Closure(CheckboxItem):string|null */
+    private ?Closure $itemFormatter = null;
+    /** @var array<array-key, string> */
+    private array $items = [];
+    private array $itemsAttributes = [];
+    /** @var bool|float|int|string|Stringable|null */
+    private $uncheckValue = false;
+    private string $unselect = '';
+
+    /**
+     * The container attributes for generating the list of checkboxes tag using {@see CheckBoxList}.
+     *
+     * @param array $value
+     *
+     * @return static
+     */
+    public function containerAttributes(array $value): self
+    {
+        $new = clone $this;
+        $new->containerAttributes = $value;
+        return $new;
+    }
+
+    /**
+     * The tag name for the container element.
+     *
+     * @param string $value tag name.
+     *
+     * @return static
+     */
+    public function containerTag(?string $name): self
+    {
+        $new = clone $this;
+        $new->containerTag = $name;
+        return $new;
+    }
+
+    /**
+     * Set whether the element is disabled or not.
+     *
+     * If this attribute is set to `true`, the element is disabled. Disabled elements are usually drawn with grayed-out
+     * text.
+     * If the element is disabled, it does not respond to user actions, it cannot be focused, and the command event
+     * will not fire. In the case of form elements, it will not be submitted. Do not set the attribute to true, as
+     * this will suggest you can set it to false to enable the element again, which is not the case.
+     *
+     * @param bool $value
+     *
+     * @return static
+     */
+    public function disabled(bool $value = true): self
+    {
+        $new = clone $this;
+        $new->itemsAttributes['disabled'] = $value;
+        return $new;
+    }
+
+    /**
+     * Callable, a callback that can be used to customize the generation of the HTML code corresponding to a single
+     * item in $items.
+     *
+     * The signature of this callback must be:
+     *
+     * ```php
+     * function ($index, $label, $name, $checked, $value)
+     * ```
+     *
+     * @param Closure(CheckboxItem):string|null $formatter
+     *
+     * @return static
+     */
+    public function itemFormater(?Closure $formatter): self
+    {
+        $new = clone $this;
+        $new->itemFormatter = $formatter;
+        return $new;
+    }
+
+    /**
+     * The data used to generate the list of checkboxes.
+     *
+     * The array keys are the list of checkboxes values, and the array values are the corresponding labels.
+     *
+     * Note that the labels will NOT be HTML-encoded, while the values will.
+     *
+     * @param array<array-key, string> $value
+     *
+     * @return static
+     */
+    public function items(array $value = []): self
+    {
+        $new = clone $this;
+        $new->items = $value;
+        return $new;
+    }
+
+    /**
+     * The items atrributes for generating the list of checkboxes tag using {@see CheckBoxList}.
+     *
+     * @param array $value
+     *
+     * @return static
+     */
+    public function itemsAttributes(array $value = []): self
+    {
+        $new = clone $this;
+        $new->itemsAttributes = $value;
+        return $new;
+    }
+
+    /**
+     * The value associated with the uncheck state of the checkboxlist.
+     *
+     * When this attribute is present, a hidden input will be generated so that if the checkboxlist is not checked and
+     * is submitted, the value of this attribute will still be submitted to the server via the hidden input.
+     *
+     * @param bool|float|int|string|Stringable|null $value
+     *
+     * @return static
+     */
+    public function uncheckValue($value): self
+    {
+        $new = clone $this;
+        $new->uncheckValue = $value;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/sec-forms.html#the-readonly-attribute
+     *
+     * @return static
+     */
+    public function readonly(): self
+    {
+        $new = clone $this;
+        $new->itemsAttributes['readonly'] = true;
+        return $new;
+    }
+
+    /**
+     * The HTML code that separates items.
+     *
+     * @param string $value
+     *
+     * @return static
+     */
+    public function separator(string $value): self
+    {
+        $new = clone $this;
+        $new->attributes['separator'] = $value;
+        return $new;
+    }
+
+    public function withoutContainer(): self
+    {
+        $new = clone $this;
+        $new->containerTag = null;
+        return $new;
+    }
+
+    /**
+     * @return string the generated checkbox list.
+     */
+    protected function run(): string
+    {
+        $new = clone $this;
+
+        $checkboxList = ChecboxListWidget::create($new->getInputName());
+
+        $new->containerAttributes['id'] = $new->containerAttributes['id'] ?? $new->getId();
+
+        /** @var null|scalar|Stringable|iterable<int, Stringable|scalar> */
+        $values = $new->getValue();
+
+        /** @var string */
+        $separator = $new->attributes['separator'] ?? '';
+
+        /** @var string|null */
+        $uncheckValue = $new->attributes['uncheckValue'] ?? $new->uncheckValue;
+
+        unset($new->attributes['itemsAttributes'], $new->attributes['separator']);
+
+        if ($separator !== '') {
+            $checkboxList = $checkboxList->separator($separator);
+        }
+
+        if (is_iterable($values)) {
+            $checkboxList = $checkboxList->values($values);
+        } elseif (is_scalar($values)) {
+            $checkboxList = $checkboxList->value($values);
+        }
+
+        return $checkboxList
+            ->checkboxAttributes($new->attributes)
+            ->containerAttributes($new->containerAttributes)
+            ->containerTag($new->containerTag)
+            ->itemFormatter($new->itemFormatter)
+            ->items($new->items)
+            ->replaceCheckboxAttributes($new->itemsAttributes)
+            ->uncheckValue($uncheckValue)
+            ->render();
+    }
+}
---
+++ tests/Widget/CheckboxListTest.php
@@ -0,0 +1,212 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Form\Tests\Widget;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Form\Tests\Stub\PersonalForm;
+use Yiisoft\Form\Tests\TestSupport\TestTrait;
+use Yiisoft\Form\Widget\CheckBoxList;
+use Yiisoft\Html\Widget\CheckboxList\CheckboxItem;
+use Yiisoft\Test\Support\Container\SimpleContainer;
+use Yiisoft\Widget\WidgetFactory;
+
+final class CheckboxListTest extends TestCase
+{
+    use TestTrait;
+
+    public function testContainerAttributes(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(0);
+
+        $html = CheckboxList::widget()
+            ->config($data, 'sex')
+            ->containerAttributes(['class' => 'test-class'])
+            ->items(['Female', 'Male'])
+            ->render();
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <div id="personalform-sex" class="test-class">
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0" checked> Female</label>
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="1"> Male</label>
+        </div>
+        HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testContainerTag(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(0);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <tag-test id="personalform-sex">
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0" checked> Female</label>
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="1"> Male</label>
+        </tag-test>
+        HTML;
+        $this->assertEqualsWithoutLE(
+            $expected,
+            CheckboxList::widget()->config($data, 'sex')->containerTag('tag-test')->items(['Female', 'Male'])->render(),
+        );
+    }
+
+    public function testDisabled(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(0);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="" disabled>
+        <div id="personalform-sex">
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0" checked disabled> Female</label>
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="1" disabled> Male</label>
+        </div>
+        HTML;
+        $this->assertEqualsWithoutLE(
+            $expected,
+            CheckboxList::widget()->config($data, 'sex')->items(['Female', 'Male'])->disabled()->render(),
+        );
+    }
+
+    public function testItemsAttributes(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(0);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <div id="personalform-sex">
+        <label><input type="checkbox" class="test-class" name="PersonalForm[sex][]" value="0" checked> Female</label>
+        <label><input type="checkbox" class="test-class" name="PersonalForm[sex][]" value="1"> Male</label>
+        </div>
+        HTML;
+        $html = CheckboxList::widget()
+            ->config($data, 'sex')
+            ->items(['Female', 'Male'])
+            ->itemsAttributes(['class' => 'test-class'])
+            ->render();
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testItemFormater(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(0);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <div id="personalform-sex">
+        <div class='test-class'><label><input tabindex='0' class='test-class' type='checkbox' checked name='PersonalForm[sex][]' value='0'> Female</label></div>
+        <div class='test-class'><label><input tabindex='1' class='test-class' type='checkbox'  name='PersonalForm[sex][]' value='1'> Male</label></div>
+        </div>
+        HTML;
+        $html = CheckboxList::widget()
+            ->config($data, 'sex')
+            ->items(['Female', 'Male'])
+            ->itemFormater(static function (CheckboxItem $item) {
+                $check = $item->checked ? 'checked' : '';
+                return "<div class='test-class'><label><input tabindex='{$item->index}' class='test-class' type='checkbox' {$check} name='{$item->name}' value='{$item->value}'> {$item->label}</label></div>";
+            })
+            ->render();
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    public function testRender(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(1);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <div id="personalform-sex">
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0"> Female</label>
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="1" checked> Male</label>
+        </div>
+        HTML;
+        $this->assertEqualsWithoutLE(
+            $expected,
+            CheckboxList::widget()->config($data, 'sex')->items(['Female', 'Male'])->render(),
+        );
+    }
+
+    public function testSeparator(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(1);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <div id="personalform-sex">
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0"> Female</label>&#9866;<label><input type="checkbox" name="PersonalForm[sex][]" value="1" checked> Male</label>
+        </div>
+        HTML;
+        $this->assertEqualsWithoutLE(
+            $expected,
+            CheckboxList::widget()->config($data, 'sex')->items(['Female', 'Male'])->separator('&#9866;')->render(),
+        );
+    }
+
+    public function testReadOnly(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(1);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[sex]" value="">
+        <div id="personalform-sex">
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0" readonly> Female</label>
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="1" checked readonly> Male</label>
+        </div>
+        HTML;
+        $this->assertEqualsWithoutLE(
+            $expected,
+            CheckboxList::widget()->config($data, 'sex')->items(['Female', 'Male'])->readOnly()->render(),
+        );
+    }
+
+    public function testValueIterable(): void
+    {
+        $data = new PersonalForm();
+        $data->setAttribute('citiesVisited', [0, 1]);
+
+        $expected = <<<'HTML'
+        <input type="hidden" name="PersonalForm[citiesVisited]" value="">
+        <div id="personalform-citiesvisited">
+        <label><input type="checkbox" name="PersonalForm[citiesVisited][]" value="0" checked> Moscu</label>
+        <label><input type="checkbox" name="PersonalForm[citiesVisited][]" value="1" checked> San Petesburgo</label>
+        </div>
+        HTML;
+        $this->assertEqualsWithoutLE(
+            $expected,
+            CheckboxList::widget()->config($data, 'citiesVisited')->items(['Moscu', 'San Petesburgo'])->render(),
+        );
+    }
+
+    public function testWithoutContainer(): void
+    {
+        $data = new PersonalForm();
+        $data->sex(1);
+
+        $html = CheckboxList::widget()
+            ->config($data, 'sex')
+            ->items(['Female', 'Male'])
+            ->uncheckValue(null)
+            ->withoutContainer()
+            ->render();
+        $expected = <<<'HTML'
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="0"> Female</label>
+        <label><input type="checkbox" name="PersonalForm[sex][]" value="1" checked> Male</label>
+        HTML;
+        $this->assertEqualsWithoutLE($expected, $html);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        WidgetFactory::initialize(new SimpleContainer(), []);
+    }
+}
