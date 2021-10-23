64b6e66f8b4ce7e8008cbd81e82a18d164753133
Wilmer Arambula <terabytesoftw@users.noreply.github.com>
2021-10-23 20:03:43 +0000

    Apply fixes from StyleCI

--- src/Widget/Number.php
+++ src/Widget/Number.php
@@ -80,7 +80,7 @@ final class Number extends Widget
         /** @link https://www.w3.org/TR/2012/WD-html-markup-20120329/input.number.html#input.number.attrs.value */
         $value = HtmlForm::getRawAttributeValue($new->getFormModel(), $new->attribute);
 
-        if (!is_numeric($value) && !is_null($value)) {
+        if (!is_numeric($value) && null !== $value) {
             throw new InvalidArgumentException('Number widget must be a numeric or null value.');
         }
 
