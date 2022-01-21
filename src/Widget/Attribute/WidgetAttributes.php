4181af8d90dbda3143cdbfc84a59f9660849aae5
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-01-21 13:44:58 -0300

    Apply fixes from StyleCI (#63)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Widget/Attribute/WidgetAttributes.php
+++ src/Widget/Attribute/WidgetAttributes.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Form\Widget\Attribute;
 
-use InvalidArgumentException;
 use Stringable;
 use Yiisoft\Form\Exception\AttributeNotSetException;
 use Yiisoft\Form\Exception\FormModelNotSetException;
--- src/Widget/FieldPart/Error.php
+++ src/Widget/FieldPart/Error.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Form\Widget\FieldPart;
 
-use InvalidArgumentException;
 use Yiisoft\Form\Exception\AttributeNotSetException;
 use Yiisoft\Form\Exception\FormModelNotSetException;
 use Yiisoft\Form\FormModelInterface;
