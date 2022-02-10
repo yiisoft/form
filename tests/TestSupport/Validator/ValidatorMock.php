6d75cbc851c21fb55214be21aa90b3ca3c2ef9d6
Alexey Rogachev <arogachev90@gmail.com>
2022-02-10 16:49:26 +0600

    Adapt after removing result set in validator (#178)

--- tests/TestSupport/Validator/ValidatorMock.php
+++ tests/TestSupport/Validator/ValidatorMock.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Tests\TestSupport\Validator;
 
 use Yiisoft\Validator\Formatter;
-use Yiisoft\Validator\ResultSet;
+use Yiisoft\Validator\Result;
 use Yiisoft\Validator\Validator;
 use Yiisoft\Validator\ValidatorInterface;
 
@@ -18,7 +18,7 @@ final class ValidatorMock implements ValidatorInterface
         $this->validator = new Validator(new Formatter());
     }
 
-    public function validate($dataSet, iterable $rules = []): ResultSet
+    public function validate($dataSet, iterable $rules = []): Result
     {
         return $this->validator->validate($dataSet, $rules);
     }
