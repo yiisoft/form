73701c38132f155c68203b328bb3599c6113b749
Wilmer <terabytesoftw@gmail.com>
2022-03-16 10:41:04 -0300

    Add test complete with raw data.

--- tests/TestSupport/Form/NestedForm.php
+++ tests/TestSupport/Form/NestedForm.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Tests\TestSupport\Form;
 
 use Yiisoft\Form\FormModel;
+use Yiisoft\Validator\Rule\Required;
 use Yiisoft\Validator\Rule\Number;
 
 final class NestedForm extends FormModel
 {
-    public ?int $number = null;
+    private ?string $id = null;
+    private ?int $number = null;
 
     public function getRules(): array
     {
         return [
+            'id' => [Required::rule()],
             'number' => [Number::rule()->integer()->min(1)],
         ];
     }
--- tests/Validator/NestedTest.php
+++ tests/Validator/NestedTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Tests\Validator;
 
 use PHPUnit\Framework\TestCase;
+use Yiisoft\Form\Helper\HtmlFormErrors;
 use Yiisoft\Form\Tests\TestSupport\Form\MainFormNested;
 use Yiisoft\Form\Tests\TestSupport\TestTrait;
 
@@ -20,14 +21,61 @@ final class NestedTest extends TestCase
         $form->load(
             [
                 'MainFormNested' => [
-                    'value' => 'main-form',
+                    'nestedForm.id' => '1',
                     'nestedForm.number' => 2,
+                    'value' => 'main-form',
                 ],
             ],
         );
 
-        $this->assertSame('main-form', $form->getAttributeValue('value')); // main-form
+        $this->assertSame('1', $form->getAttributeValue('nestedForm.id')); // main-form
         $this->assertSame(2, $form->getAttributeValue('nestedForm.number')); // 2
-        $this->assertTrue($validator->validate($form)->isValid()); // should return true, returns false.
+        $this->assertSame('main-form', $form->getAttributeValue('value')); // main-form
+        $this->assertTrue($validator->validate($form)->isValid()); // return true
+        $this->assertEmpty(HtmlFormErrors::getAllErrors($form));
+    }
+
+    public function testAttributeNestedValidationInvalidId(): void
+    {
+        $form = new MainFormNested();
+
+        $validator = $this->createValidatorMock();
+        $form->load(
+            [
+                'MainFormNested' => [
+                    'nestedForm.id' => '',
+                    'nestedForm.number' => 2,
+                    'value' => 'main-form',
+                ],
+            ],
+        );
+
+        $this->assertSame('', $form->getAttributeValue('nestedForm.id')); // main-form
+        $this->assertSame(2, $form->getAttributeValue('nestedForm.number')); // 2
+        $this->assertSame('main-form', $form->getAttributeValue('value')); // main-form
+        $this->assertFalse($validator->validate($form)->isValid()); // return true
+        $this->assertSame(['nestedForm.number' => ['Value cannot be blank.']], HtmlFormErrors::getAllErrors($form));
+    }
+
+    public function testAttributeNestedValidationInvalidNumber(): void
+    {
+        $form = new MainFormNested();
+
+        $validator = $this->createValidatorMock();
+        $form->load(
+            [
+                'MainFormNested' => [
+                    'nestedForm.id' => '1',
+                    'nestedForm.number' => 'x',
+                    'value' => 'main-form',
+                ],
+            ],
+        );
+
+        $this->assertSame('1', $form->getAttributeValue('nestedForm.id')); // main-form
+        $this->assertSame('x', $form->getAttributeValue('nestedForm.number')); // 2
+        $this->assertSame('main-form', $form->getAttributeValue('value')); // main-form
+        $this->assertFalse($validator->validate($form)->isValid()); // return true
+        $this->assertSame(['nestedForm.number' => ['Value must be an integer.']], HtmlFormErrors::getAllErrors($form));
     }
 }
