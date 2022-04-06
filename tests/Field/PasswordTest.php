ca21bc34f10eedd0d81716a6ab24a635121156e2
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2022-04-06 15:52:38 +0300

    test

---
+++ tests/Field/PasswordTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Form\Tests\Field;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Form\Field\Password;
+use Yiisoft\Form\Tests\Support\AssertTrait;
+use Yiisoft\Form\Tests\Support\Form\PasswordForm;
+use Yiisoft\Test\Support\Container\SimpleContainer;
+use Yiisoft\Widget\WidgetFactory;
+
+final class PasswordTest extends TestCase
+{
+    use AssertTrait;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        WidgetFactory::initialize(new SimpleContainer());
+    }
+
+    public function testBase(): void
+    {
+        $result = Password::widget()->attribute(new PasswordForm(), 'old')->render();
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div>
+            <label for="passwordform-old">Old password</label>
+            <input type="password" id="passwordform-old" name="PasswordForm[old]" value>
+            <div>Enter your old password.</div>
+            </div>
+            HTML,
+            $result
+        );
+    }
+}
