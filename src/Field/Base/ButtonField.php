78f425f65894c84127fb5e0a60a859f626503953
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-04-19 22:11:42 +0300

    Add tabIndex()

--- src/Field/Base/ButtonField.php
+++ src/Field/Base/ButtonField.php
@@ -59,6 +59,30 @@ abstract class ButtonField extends PartsField
         return $new;
     }
 
+    /**
+     * The `tabindex` attribute indicates that its element can be focused, and where it participates in sequential
+     * keyboard navigation (usually with the Tab key, hence the name).
+     *
+     * It accepts an integer as a value, with different results depending on the integer's value:
+     *
+     * - A negative value (usually `tabindex="-1"`) means that the element is not reachable via sequential keyboard
+     *   navigation, but could be focused with Javascript or visually. It's mostly useful to create accessible widgets
+     *   with JavaScript.
+     * - `tabindex="0"` means that the element should be focusable in sequential keyboard navigation, but its order is
+     *   defined by the document's source order.
+     * - A positive value means the element should be focusable in sequential keyboard navigation, with its order
+     *   defined by the value of the number. That is, `tabindex="4"` is focused before `tabindex="5"`, but after
+     *   `tabindex="3"`.
+     *
+     * @link https://html.spec.whatwg.org/multipage/interaction.html#attr-tabindex
+     */
+    final public function tabIndex(?int $value): static
+    {
+        $new = clone $this;
+        $new->attributes['tabindex'] = $value;
+        return $new;
+    }
+
     final protected function generateInput(): string
     {
         $button = ($this->button ?? Button::tag())
--- src/Field/Image.php
+++ src/Field/Image.php
@@ -58,6 +58,30 @@ final class Image extends PartsField
         return $new;
     }
 
+    /**
+     * The `tabindex` attribute indicates that its element can be focused, and where it participates in sequential
+     * keyboard navigation (usually with the Tab key, hence the name).
+     *
+     * It accepts an integer as a value, with different results depending on the integer's value:
+     *
+     * - A negative value (usually `tabindex="-1"`) means that the element is not reachable via sequential keyboard
+     *   navigation, but could be focused with Javascript or visually. It's mostly useful to create accessible widgets
+     *   with JavaScript.
+     * - `tabindex="0"` means that the element should be focusable in sequential keyboard navigation, but its order is
+     *   defined by the document's source order.
+     * - A positive value means the element should be focusable in sequential keyboard navigation, with its order
+     *   defined by the value of the number. That is, `tabindex="4"` is focused before `tabindex="5"`, but after
+     *   `tabindex="3"`.
+     *
+     * @link https://html.spec.whatwg.org/multipage/interaction.html#attr-tabindex
+     */
+    public function tabIndex(?int $value): self
+    {
+        $new = clone $this;
+        $new->inputTagAttributes['tabindex'] = $value;
+        return $new;
+    }
+
     public function inputTagAttributes(array $attributes): self
     {
         $new = clone $this;
