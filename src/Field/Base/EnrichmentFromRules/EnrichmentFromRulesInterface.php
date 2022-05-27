4ec01803c9b23e43fcf89755bc28665b31e4b62a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-05-27 13:16:46 +0300

    Add PhpDoc

--- src/Field/Base/EnrichmentFromRules/EnrichmentFromRulesInterface.php
+++ src/Field/Base/EnrichmentFromRules/EnrichmentFromRulesInterface.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Form\Field\Base\EnrichmentFromRules;
 
+/**
+ * Fields implementing `EnrichmentFromRulesInterface` can automatically configuration based on validation rules.
+ */
 interface EnrichmentFromRulesInterface
 {
+    /**
+     * Enable/disable automatic configuration based on validation rules.
+     */
     public function enrichmentFromRules(bool $enrichment): self;
 }
