015f67ab11a6e17b55c270bb7eb4635fabcc24fb
Rustam <rmamdaminov@gmail.com>
2022-06-05 19:20:11 +0500

    Fix ValidatorMock

--- tests/TestSupport/Validator/ValidatorMock.php
+++ tests/TestSupport/Validator/ValidatorMock.php
@@ -5,25 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Form\Tests\TestSupport\Validator;
 
 use Yiisoft\Validator\Result;
-use Yiisoft\Validator\Rule\AtLeast\AtLeastHandler;
-use Yiisoft\Validator\Rule\Boolean\BooleanHandler;
-use Yiisoft\Validator\Rule\Callback\CallbackHandler;
-use Yiisoft\Validator\Rule\CompareTo\CompareToHandler;
-use Yiisoft\Validator\Rule\Count\CountHandler;
-use Yiisoft\Validator\Rule\Each\EachHandler;
-use Yiisoft\Validator\Rule\Email\EmailHandler;
-use Yiisoft\Validator\Rule\GroupRule\GroupRuleHandler;
-use Yiisoft\Validator\Rule\HasLength\HasLengthHandler;
-use Yiisoft\Validator\Rule\InRange\InRangeHandler;
-use Yiisoft\Validator\Rule\Ip\IpHandler;
-use Yiisoft\Validator\Rule\Json\JsonHandler;
-use Yiisoft\Validator\Rule\Nested\NestedHandler;
-use Yiisoft\Validator\Rule\Number\NumberHandler;
-use Yiisoft\Validator\Rule\Regex\RegexHandler;
-use Yiisoft\Validator\Rule\Required\RequiredHandler;
-use Yiisoft\Validator\Rule\Subset\SubsetHandler;
-use Yiisoft\Validator\Rule\Url\UrlHandler;
-use Yiisoft\Validator\StaticRuleHandlerResolver;
+use Yiisoft\Validator\SimpleRuleHandlerContainer;
 use Yiisoft\Validator\Validator;
 use Yiisoft\Validator\ValidatorInterface;
 
@@ -33,30 +15,11 @@ final class ValidatorMock implements ValidatorInterface
 
     public function __construct()
     {
-        $this->validator = new Validator(new StaticRuleHandlerResolver([
-            AtLeastHandler::class => new AtLeastHandler(),
-            BooleanHandler::class => new BooleanHandler(),
-            CallbackHandler::class => new CallbackHandler(),
-            CompareToHandler::class => new CompareToHandler(),
-            CountHandler::class => new CountHandler(),
-            EachHandler::class => new EachHandler(),
-            EmailHandler::class => new EmailHandler(),
-            GroupRuleHandler::class => new GroupRuleHandler(),
-            HasLengthHandler::class => new HasLengthHandler(),
-            InRangeHandler::class => new InRangeHandler(),
-            IpHandler::class => new IpHandler(),
-            JsonHandler::class => new JsonHandler(),
-            NumberHandler::class => new NumberHandler(),
-            RegexHandler::class => new RegexHandler(),
-            RequiredHandler::class => new RequiredHandler(),
-            SubsetHandler::class => new SubsetHandler(),
-            UrlHandler::class => new UrlHandler(),
-            NestedHandler::class => new NestedHandler(),
-        ]));
+        $this->validator = new Validator(new SimpleRuleHandlerContainer());
     }
 
-    public function validate($dataSet, iterable $rules = []): Result
+    public function validate($data, iterable $rules = []): Result
     {
-        return $this->validator->validate($dataSet, $rules);
+        return $this->validator->validate($data, $rules);
     }
 }
