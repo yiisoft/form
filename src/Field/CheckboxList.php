9e5b942037a4a5371f0ae0c9948fecb58441f9ee
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-11-27 20:45:28 +0300

    fix

--- src/Field/CheckboxList.php
+++ src/Field/CheckboxList.php
@@ -38,7 +38,7 @@ final class CheckboxList extends PartsField implements ValidationClassInterface
     public function checkboxAttributes(array $attributes): self
     {
         $new = clone $this;
-        $new->widget = $this->widget->replaceCheckboxAttributes($attributes);
+        $new->widget = $this->widget->checkboxAttributes($attributes);
         return $new;
     }
 
@@ -55,7 +55,7 @@ final class CheckboxList extends PartsField implements ValidationClassInterface
     public function individualInputAttributes(array $attributes): self
     {
         $new = clone $this;
-        $new->widget = $this->widget->replaceIndividualInputAttributes($attributes);
+        $new->widget = $this->widget->individualInputAttributes($attributes);
         return $new;
     }
 
