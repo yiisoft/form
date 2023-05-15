b19b2b01c001a24eb0adcecfb2102e325e29ce3f
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-15 15:51:12 +0300

    Configure `FormHydrator`

---
+++ config/di.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Vjik\InputValidation\ValidatingHydrator;
+use Yiisoft\Form\FormHydrator;
+
+return [
+    FormHydrator::class => [
+        '__construct()' => [
+            'hydrator' => ValidatingHydrator::class
+        ],
+    ],
+];
