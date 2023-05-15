7fd16822d87230c64f5280d75001658477ab5010
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-15 15:53:24 +0300

    fix

--- config/di.php
+++ config/di.php
@@ -3,12 +3,13 @@
 declare(strict_types=1);
 
 use Vjik\InputValidation\ValidatingHydrator;
+use Yiisoft\Definitions\Reference;
 use Yiisoft\Form\FormHydrator;
 
 return [
     FormHydrator::class => [
         '__construct()' => [
-            'hydrator' => ValidatingHydrator::class
+            'hydrator' => Reference::to(ValidatingHydrator::class),
         ],
     ],
 ];
