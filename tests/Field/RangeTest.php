49ff65abe6736cfe3b255169744501a9d87ae559
vjik <vjik@users.noreply.github.com>
2023-12-01 21:39:34 +0000

    Apply Rector changes (CI)

--- tests/Field/RangeTest.php
+++ tests/Field/RangeTest.php
@@ -57,12 +57,12 @@ final class RangeTest extends TestCase
             ->addOutputAttributes(['id' => 'UID'])
             ->render();
 
-        $expected = <<<HTML
-            <div>
-            <input type="range" name="volume" value="23" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
-            <span id="UID" class="red">23</span>
-            </div>
-            HTML;
+        $expected = <<<HTML_WRAP
+<div>
+<input type="range" name="volume" value="23" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
+<span id="UID" class="red">23</span>
+</div>
+HTML_WRAP;
 
         $this->assertSame($expected, $result);
     }
@@ -77,12 +77,12 @@ final class RangeTest extends TestCase
             ->outputAttributes(['id' => 'UID'])
             ->render();
 
-        $expected = <<<HTML
-            <div>
-            <input type="range" name="volume" value="23" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
-            <span id="UID">23</span>
-            </div>
-            HTML;
+        $expected = <<<HTML_WRAP
+<div>
+<input type="range" name="volume" value="23" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
+<span id="UID">23</span>
+</div>
+HTML_WRAP;
 
         $this->assertSame($expected, $result);
     }
@@ -98,12 +98,12 @@ final class RangeTest extends TestCase
             ->outputAttributes(['id' => 'UID'])
             ->render();
 
-        $expected = <<<HTML
-            <div>
-            <input type="range" name="volume" value="23" min="1" max="100" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
-            <span id="UID">23</span>
-            </div>
-            HTML;
+        $expected = <<<HTML_WRAP
+<div>
+<input type="range" name="volume" value="23" min="1" max="100" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
+<span id="UID">23</span>
+</div>
+HTML_WRAP;
 
         $this->assertSame($expected, $result);
     }
@@ -118,12 +118,12 @@ final class RangeTest extends TestCase
             ->outputAttributes(['id' => 'UID'])
             ->render();
 
-        $expected = <<<HTML
-            <div>
-            <input type="range" name="volume" value="23" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
-            <div id="UID">23</div>
-            </div>
-            HTML;
+        $expected = <<<HTML_WRAP
+<div>
+<input type="range" name="volume" value="23" oninput="document.getElementById(&quot;UID&quot;).innerHTML=this.value">
+<div id="UID">23</div>
+</div>
+HTML_WRAP;
 
         $this->assertSame($expected, $result);
     }
