23576d1d69f064a09fa1463f1b066cb6bdf22645
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-03 19:39:28 +0300

    5

--- tests/PureFieldTest.php
+++ tests/PureFieldTest.php
@@ -6,6 +6,7 @@ namespace Yiisoft\Form\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Form\PureField;
+use Yiisoft\Form\Tests\Support\ThemedPureField;
 use Yiisoft\Form\ThemeContainer;
 use Yiisoft\Html\Tag\Button;
 use Yiisoft\Test\Support\Container\SimpleContainer;
@@ -46,6 +47,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testButtonWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::button(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <button type="button"></button>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testButtonGroup(): void
     {
         $html = PureField::buttonGroup()->buttons(Button::tag())->render();
@@ -59,6 +79,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testButtonGroupWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::buttonGroup(theme: 'test')->buttons(Button::tag())->render();
+
+        $expected = <<<HTML
+            <span>
+            <button></button>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testCheckbox(): void
     {
         $html = PureField::checkbox()->render();
@@ -67,6 +106,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="checkbox" value="1">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testCheckboxGroupWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::checkbox(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="checkbox" value="1">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -91,6 +149,31 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testCheckboxListWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::checkboxList(theme: 'test')
+            ->name('test')
+            ->itemsFromValues(['a', 'b'])
+            ->render();
+
+        $expected = <<<HTML
+            <span>
+            <div>
+            <label><input type="checkbox" name="test[]" value="a"> a</label>
+            <label><input type="checkbox" name="test[]" value="b"> b</label>
+            </div>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testDate(): void
     {
         $html = PureField::date()->render();
@@ -99,6 +182,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="date">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testDateWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::date(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="date">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -117,6 +219,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testDateTimeWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::dateTime(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="datetime">
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testDateTimeLocal(): void
     {
         $html = PureField::dateTimeLocal()->render();
@@ -130,6 +251,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testDateTimeLocalWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::dateTimeLocal(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="datetime-local">
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testEmail(): void
     {
         $html = PureField::email()->render();
@@ -138,6 +278,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="email">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testEmailWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::email(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="email">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -157,6 +316,26 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testFieldsetWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::fieldset(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <fieldset>
+            </fieldset>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testFile(): void
     {
         $html = PureField::file()->render();
@@ -170,6 +349,44 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testFileWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::file(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="file">
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testHidden(): void
+    {
+        $html = PureField::hidden()->render();
+        $this->assertSame('<input type="hidden">', $html);
+    }
+
+    public function testHiddenWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'inputClass' => 'green'
+            ],
+        ]);
+
+        $html = ThemedPureField::hidden(theme: 'test')->render();
+        
+        $this->assertSame('<input type="hidden" class="green">', $html);
+    }
+
     public function testImage(): void
     {
         $html = PureField::image()->render();
@@ -178,6 +395,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="image">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testImageWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::image(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="image">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -196,6 +432,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testNumberWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::number(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="number">
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testPassword(): void
     {
         $html = PureField::password()->render();
@@ -204,6 +459,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="password">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testPasswordWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::password(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="password">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -228,6 +502,31 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testRadioListWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::radioList(theme: 'test')
+            ->name('test')
+            ->itemsFromValues(['a', 'b'])
+            ->render();
+
+        $expected = <<<HTML
+            <span>
+            <div>
+            <label><input type="radio" name="test" value="a"> a</label>
+            <label><input type="radio" name="test" value="b"> b</label>
+            </div>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testRange(): void
     {
         $html = PureField::range()->render();
@@ -236,6 +535,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="range">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testRangeWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::range(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="range">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -267,6 +585,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testResetButtonWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::resetButton(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <button type="reset"></button>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testSelect(): void
     {
         $html = PureField::select()->render();
@@ -275,6 +612,25 @@ final class PureFieldTest extends TestCase
             <div>
             <select></select>
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testSelectWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::select(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <select></select>
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -306,6 +662,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testSubmitButtonWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::submitButton(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <button type="submit"></button>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testTelephone(): void
     {
         $html = PureField::telephone()->render();
@@ -314,6 +689,57 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="tel">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testTelephoneWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::telephone(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="tel">
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testText(): void
+    {
+        $html = PureField::text()->render();
+
+        $expected = <<<HTML
+            <div>
+            <input type="text">
+            </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testTextWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::text(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="text">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -332,6 +758,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testTextareaWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::textarea(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <textarea></textarea>
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testTime(): void
     {
         $html = PureField::time()->render();
@@ -340,6 +785,25 @@ final class PureFieldTest extends TestCase
             <div>
             <input type="time">
             </div>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testTimeWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::time(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="time">
+            </span>
             HTML;
 
         $this->assertSame($expected, $html);
@@ -358,6 +822,25 @@ final class PureFieldTest extends TestCase
         $this->assertSame($expected, $html);
     }
 
+    public function testUrlWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'containerTag' => 'span'
+            ],
+        ]);
+
+        $html = ThemedPureField::url(theme: 'test')->render();
+
+        $expected = <<<HTML
+            <span>
+            <input type="url">
+            </span>
+            HTML;
+
+        $this->assertSame($expected, $html);
+    }
+
     public function testLabel(): void
     {
         $html = PureField::label()->render();
@@ -368,6 +851,19 @@ final class PureFieldTest extends TestCase
     {
         $html = PureField::label('test')->render();
         $this->assertSame('<label>test</label>', $html);
+    }
+
+    public function testLabelWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'labelClass' => 'red'
+            ],
+        ]);
+
+        $html = ThemedPureField::label('hello', theme: 'test')->render();
+
+        $this->assertSame('<label class="red">hello</label>', $html);
     }
 
     public function testHint(): void
@@ -382,6 +878,19 @@ final class PureFieldTest extends TestCase
         $this->assertSame('<div>test</div>', $html);
     }
 
+    public function testHintWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'hintClass' => 'red'
+            ],
+        ]);
+
+        $html = ThemedPureField::hint('hello', theme: 'test')->render();
+
+        $this->assertSame('<div class="red">hello</div>', $html);
+    }
+
     public function testError(): void
     {
         $html = PureField::error()->render();
@@ -392,5 +901,18 @@ final class PureFieldTest extends TestCase
     {
         $html = PureField::error('test')->render();
         $this->assertSame('<div>test</div>', $html);
+    }
+
+    public function testErrorWithTheme(): void
+    {
+        ThemeContainer::initialize([
+            'test' => [
+                'errorClass' => 'red'
+            ],
+        ]);
+
+        $html = ThemedPureField::error('hello', theme: 'test')->render();
+
+        $this->assertSame('<div class="red">hello</div>', $html);
     }
 }
